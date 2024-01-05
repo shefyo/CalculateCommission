277e904f650829b11374c91281de736291deaf27
Netology <olegshefner6@gmail.com>
2024-01-05 20:00:35 +0300

    Initial commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,26 @@
+fun main() {
+    var transfer = calculateCommission("Maestro", 100000, 5000)
+    println(transfer)
+}
+
+fun calculateCommission(cardType: String = "VK Pay", previousTransfers: Int = 0, transferAmount: Int): Int {
+    return when (cardType) {
+        "Mastercard", "Maestro" -> {
+            if (previousTransfers > 75000) {
+                (transferAmount - 20 - (0.6 * transferAmount) / 100).toInt()
+            } else {
+                0
+            }
+        }
+
+        "Visa", "Мир" -> {
+            if (transferAmount * 0.75 < 35) {
+                (transferAmount - 35).toInt()
+            } else {
+                (transferAmount - (0.75 * transferAmount) / 100).toInt()
+            }
+        }
+
+        else -> 0
+    }
+}
