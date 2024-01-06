13eb9639d38475b274f49bcbcc35ea3db3fdb220
shefyo <110970148+shefyo@users.noreply.github.com>
2024-01-06 21:02:52 +0300

    Изменение системы лимитов

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,32 +1,28 @@
 fun main() {
-    calculateCommission("Maestro", 100000, 80000)
+    var transfer = calculateCommission("Maestro", 100000, 150100)
+    println(transfer)
 }
 
-fun calculateCommission(cardType: String = "VK Pay", previousTransfers: Int = 0, transferAmount: Int) {
-    if (transferAmount > 150000 && cardType != "VK Pay" || transferAmount > 15000 && cardType == "VK Pay") {
-        println("Превышение лимита")
-    } else {
-        when (cardType) {
-            "Mastercard", "Maestro" -> {
-                if (previousTransfers > 75000) {
-                    val withCommission = (transferAmount + 20 + (0.6 * transferAmount) / 100).toInt()
-                    println(withCommission)
-                } else {
-                    println(transferAmount)
-                }
+fun calculateCommission(cardType: String = "VK Pay", previousTransfers: Int = 0, transferAmount: Int): Int {
+    if (transferAmount > 150000 && cardType != "VK Pay" || transferAmount > 15000 && cardType == "VK Pay" || previousTransfers + transferAmount > 600000 && cardType != "VK Pay" || previousTransfers + transferAmount > 40000 && cardType == "VK Pay") {
+        return -1
+    } else return when (cardType) {
+        "Mastercard", "Maestro" -> {
+            if (previousTransfers + transferAmount > 75000) {
+                (transferAmount + 20 + (0.6 * transferAmount) / 100).toInt()
+            } else {
+                0
             }
-
-            "Visa", "Мир" -> {
-                if (transferAmount * 0.75 < 35) {
-                    val withCommission = (transferAmount + 35).toInt()
-                    println(withCommission)
-                } else {
-                    val withCommission = (transferAmount + (0.75 * transferAmount) / 100).toInt()
-                    println(withCommission)
-                }
-            }
-
-            else -> println(transferAmount)
         }
+
+        "Visa", "Мир" -> {
+            if (transferAmount * 0.75 < 35) {
+                (transferAmount + 35).toInt()
+            } else {
+                (transferAmount + (0.75 * transferAmount) / 100).toInt()
+            }
+        }
+
+        else -> 0
     }
 }
