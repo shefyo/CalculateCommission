bc63b16bc769d281149b293092aef3402f81fe2f
shefyo <110970148+shefyo@users.noreply.github.com>
2024-01-06 20:09:09 +0300

    Добавлены лимиты

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,26 +1,32 @@
 fun main() {
-    var transfer = calculateCommission("Maestro", 100000, 5000)
-    println(transfer)
+    calculateCommission("Maestro", 100000, 80000)
 }
 
-fun calculateCommission(cardType: String = "VK Pay", previousTransfers: Int = 0, transferAmount: Int): Int {
-    return when (cardType) {
-        "Mastercard", "Maestro" -> {
-            if (previousTransfers + transferAmount > 75000) {
-                (transferAmount - 20 - (0.6 * transferAmount) / 100).toInt()
-            } else {
-                0
+fun calculateCommission(cardType: String = "VK Pay", previousTransfers: Int = 0, transferAmount: Int) {
+    if (transferAmount > 150000 && cardType != "VK Pay" || transferAmount > 15000 && cardType == "VK Pay") {
+        println("Превышение лимита")
+    } else {
+        when (cardType) {
+            "Mastercard", "Maestro" -> {
+                if (previousTransfers > 75000) {
+                    val withCommission = (transferAmount + 20 + (0.6 * transferAmount) / 100).toInt()
+                    println(withCommission)
+                } else {
+                    println(transferAmount)
+                }
             }
-        }
 
-        "Visa", "Мир" -> {
-            if (transferAmount * 0.75 < 35) {
-                (transferAmount - 35).toInt()
-            } else {
-                (transferAmount - (0.75 * transferAmount) / 100).toInt()
+            "Visa", "Мир" -> {
+                if (transferAmount * 0.75 < 35) {
+                    val withCommission = (transferAmount + 35).toInt()
+                    println(withCommission)
+                } else {
+                    val withCommission = (transferAmount + (0.75 * transferAmount) / 100).toInt()
+                    println(withCommission)
+                }
             }
-        }
 
-        else -> 0
+            else -> println(transferAmount)
+        }
     }
 }
